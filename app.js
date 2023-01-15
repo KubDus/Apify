5ab58b4f8e2e8529894d6db1ba4c49c6d4aff96f
Jakub Dus <jakub.dus@seznam.czgit config --global user.email jakub.dus@seznam.cz>
2023-01-15 15:34:17 +0100

    Basic HTML, Items list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import Items from "./items.js";
 
 const app = express();
 app.set("view engine", "ejs");
@@ -7,15 +8,16 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.render("home")
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
+app.post("/results", function (req, res) {
+  console.log(req.body.max);
+  console.log(Items)
+  res.render("results");
+});
 
-
-
-
-
-app.listen(3000, function(){
-    console.log("Server started on port 3000")
-})
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
