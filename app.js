e1d42b28e7d37ba0682f461edcf20ce63232460a
Jakub Dus <jakub.dus@seznam.czgit config --global user.email jakub.dus@seznam.cz>
2023-01-15 14:46:23 +0100

    server running

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// this is my start file
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000")
+})
